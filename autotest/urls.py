0b526944efddc3bfbf48e451f7195e74d3d4592d
dkwyh547 <261229090@qq.com>
2019-01-13 17:10:51 +0800

    bug管理前后端开发

--- autotest/urls.py
+++ autotest/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from apitest import views    #加入引用
 from product import proviews
+from bug import bugviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +30,5 @@ urlpatterns = [
     path('apitest_manage/', views.apitest_manage),
     path('apistep_manage/', views.apistep_manage),
     path('apis_manage/', views.apis_manage),
+    path('bug_manage/', bugviews.bug_manage),
 ]
