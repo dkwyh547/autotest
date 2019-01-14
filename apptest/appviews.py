6defb806d6a41fc263795a1ef44fbe17147bb22d
dkwyh547 <261229090@qq.com>
2019-01-14 20:50:52 +0800

    APP自动化前后端开发

---
+++ apptest/appviews.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.contrib.auth import authenticate,login
+from apptest.models import Appcase,Appcasestep
+# Create your views here.
+
+#app 用例管理
+@login_required
+def appcase_manage(request):
+    appcase_list = Appcase.objects.all()
+    username = request.session.get('user', '')  #读取浏览器登录Session
+    return render(request, "appcase_manage.html", {"user":username, "appcasesteps":appcase_list})
+
+#App 测试用例步骤
+@login_required
+def appcasestep_manage(request):
+    username = request.session.get('user', '')
+    appcasestep_list = Appcasestep.objects.all()
+    return render(request, "appcasestep_manage.html", {"user":username, "appcasesteps":appcasestep_list})
--- autotest/urls.py
+++ autotest/urls.py
@@ -20,6 +20,7 @@ from apitest import views    #加入引用
 from product import proviews
 from bug import bugviews
 from set import setviews
+from apptest import appviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,4 +35,6 @@ urlpatterns = [
     path('bug_manage/', bugviews.bug_manage),
     path('set_manage/', setviews.set_manage ),
     path('user/', setviews.set_user),
+    path('appcase_manage/', appviews.appcase_manage),
+    path('appcasestep_manage/', appviews.appcasestep_manage),
 ]
