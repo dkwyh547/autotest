d9c7674bc900388366734d1647556ed24a061b53
dkwyh547 <261229090@qq.com>
2019-01-22 21:49:45 +0800

    上传文件

--- apitest/views.py
+++ apitest/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse,HttpResponseRedirect      #加入引用
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from apitest.models import Apitest,Apistep,Apis
+import pymysql
+#import HTMLTestRunner
 
 # Create your views here.
 
@@ -57,3 +59,23 @@ def apis_manage(request):
     username = request.session.get('user', '')
     apis_list = Apis.objects.all()
     return render(request, "apis_manage.html", {"user": username, "apiss": apis_list})
+
+# 测试报告
+@login_required
+def test_report(request):
+    username = request.session.get('user', '')
+    apis_list = Apis.objects.all()
+    apis_count = Apis.objects.all().count() #统计接口数
+    db = pymysql.connect(user='root', db='autotest', passwd='123456', host='127.0.0.1')
+    cursor = db.cursor()
+    sql1 = 'SELECT count(id) FROM apitest_apis WHERE apitest_apis.apistatus=1'
+    aa = cursor.execute(sql1)
+    apis_pass_count = [row[0] for row in cursor.fetchmany(aa)][0]
+    sql2 = 'SELECT count(id) FROM apitest_apis WHERE apitest_apis.apistatus=0'
+    bb = cursor.execute(sql2)
+    apis_fail_count = [row[0] for row in cursor.fetchmany(bb)][0]
+    db.close()
+    return render(request, "report.html", {"user": username, "apiss":apis_list, "apiscounts":apis_count, "apis_pass_counts": apis_pass_count, "apis_fail_counts": apis_fail_count}) #把值赋给apiscounts变量
+
+def left(request):
+    return render(request, "left.html")
--- apptest/appviews.py
+++ apptest/appviews.py
@@ -19,3 +19,9 @@ def appcasestep_manage(request):
     username = request.session.get('user', '')
     appcasestep_list = Appcasestep.objects.all()
     return render(request, "appcasestep_manage.html", {"user":username, "appcasesteps":appcasestep_list})
+
+# App 测试报告
+@login_required
+def apptest_report(request):
+    username = request.session.get('user', '')
+    return render(request, "apptest_report.html")
